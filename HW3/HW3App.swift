1db97f7849e91934cd77868d77c467b4970b34bb
Annie <annie55079@gmail.com>
2022-11-11 18:45:34 +0800

    Initial Commit

---
+++ HW3/HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3App.swift
+//  HW3
+//
+//  Created by Yu Zhi on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
